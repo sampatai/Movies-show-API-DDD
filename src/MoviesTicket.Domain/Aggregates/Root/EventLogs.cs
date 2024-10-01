306517e190951e7e4b43ecce4d83a1fb83cdf902
sampat1 <sampat.adhikari@gmail.com>
2024-10-01 20:07:02 +0545

    added event domain

---
+++ src/MoviesTicket.Domain/Aggregates/Root/EventLogs.cs
@@ -0,0 +1,20 @@
+﻿using MoviesTicket.Domain.Aggregates.Enumerations;
+
+namespace MoviesTicket.Domain.Aggregates.Root;
+
+public class EventLogs : Entity, IAggregateRoot
+{
+    public EventLogs(string description, EventType eventType)
+    {
+        EventId = Guid.NewGuid();
+        this.Description = description;
+        this.EventType = eventType;
+        CreationTime = DateTime.Now;
+
+    }
+    public Guid EventId { get; private set; }
+    public string Description { get; private set; }
+    public EventType EventType { get; private set; }
+    public DateTime CreationTime { get; private set; }
+}
+
